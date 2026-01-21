7792c61a95b9433253a14ac17657850fb584b78e
James <james.baxter@ed.ac.uk>
2026-01-21 16:48:23 +0000

    init ParseBirds

---
+++ R/ParseBirds.R
@@ -0,0 +1,22 @@
+#' ParseBirds
+#'
+#' Cleans and maps bird names to their English common name, scientific name and
+#' taxonomy. Nomenclature is designated according to BirdLife.
+#'
+#' @param birdstring Character string containing bird name. This should be either
+#' the english common name or scientific name. If species cannot be ascertained,
+#' the best-resolution taxa will be provided.
+#'
+#' @returns A tibble with columns primary_common_name, order, phylum, genus,
+#' species, and scientific_name.
+#' @export
+#'
+#' @examples
+
+
+
+################################### Demo #######################################
+
+
+#################################### END #######################################
+################################################################################
